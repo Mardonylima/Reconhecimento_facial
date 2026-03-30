556b9111a8bf870a5724edfaaeb06d032aa3a8f9
Mardony Lima <Mardony.Lima.cj12@gmail.com>
2026-03-30 00:13:18 -0300

    The JUnit library was added to pom.xml, I created the first test file, but it's still blank.

---
+++ app/src/test/java/com/reconhecimento/CaptureTest.java
@@ -0,0 +1,16 @@
+package com.reconhecimento;
+
+import org.junit.jupiter.api.Test;
+
+public class CaptureTest {
+    
+    // Em Java você não separa por arquivo/função, você separa por método de teste.
+    // Cada método da classe pode ter vários testes independentes.
+    // Cada @Test foca em um comportamento específico. 
+    
+    @Test
+    void testMain() {
+        
+    }
+    
+}
